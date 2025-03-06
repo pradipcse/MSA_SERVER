33f97b4d996b2019d2129cf52784e26df637fb09
Pradip Pashi <pradippashi00@gmail.com>
2025-03-07 00:00:21 +0600

    ready for production

--- MSA_Server/wsgi.py
+++ MSA_Server/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MSA_Server.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MSA_Server.settings.prod')
 
 application = get_wsgi_application()
